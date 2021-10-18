e088b37bed3ce816c19c56e607583c162348da37
Christopher Terence Ng <christerng@gmail.com>
2021-10-18 23:44:07 +0800

    feat: install oh-my-zsh and set LS_COLORS

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Download and install Zsh 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Set sensible colors for `ls`
+echo '# Set sensible colors for `ls`' >> .zshrc
+echo "export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'" >> .zshrc
+
+
